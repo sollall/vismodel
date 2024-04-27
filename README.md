ae31ed17946ae1d8c036b9ad13584ac87ad92503
nak1133 <la.bianca.neve26@gmail.com>
2024-04-27 11:49:42 +0900

    :+1: リポジトリのセットアップ

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-ニューラルネットワークの重み可視化
+# ニューラルネットワークの重み可視化
+
+## 環境構築
+
+```
+conda create -n vismodel python=3.10
+conda activate vismodel
+pip install -e .[dev]
+```
+
+### Dockerでの環境構築
+
+```
+cd environment && docker compose up -d
+```
+
+起動後、コンテナ内で以下のコマンドを入力する
+
+```
+cd vismodel && conda activate vismodel && pip install -e .[dev]
+```
---
+++ setup.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+import itertools
+
+from setuptools import find_packages, setup
+
+import vismodel
+
+DESCRIPTION = "visualizer for pytorch model"
+NAME = "vismodel"
+AUTHOR = "sollall, nak1133"
+AUTHOR_EMAIL = ""
+URL = "https://github.com/sollall/vismodel"
+LICENSE = "Undefined"
+DOWNLOAD_URL = "https://github.com/sollall/vismodel"
+VERSION = vismodel.__version__
+PYTHON_REQUIRES = ">=3.10"
+
+
+INSTALL_REQUIRES = []
+
+EXTRAS_REQUIRE = {
+    "dev": ["ruff == 0.4.2", "pytest == 8.1.2"],
+}
+
+all_groups = set(EXTRAS_REQUIRE.keys())
+
+PACKAGES = ["vismodel"]
+
+CLASSIFIERS = []
+
+with open("README.md", "r") as fp:
+    readme = fp.read()
+long_description = readme
+
+setup(
+    name=NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    packages=find_packages(),
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+)
