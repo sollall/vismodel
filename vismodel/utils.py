b80a2202f7242af9510ee60cca549a3d2d75b4cc
sollall <komugikoakasya@gmail.com>
2024-05-05 15:04:26 +0000

    :construction: save progress until conv1d,2d

--- vismodel/utils.py
+++ vismodel/utils.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
 def pickup_visualable_layers(model):
     """表示できるweightやbiasを持つlayerのみを持ってくる
@@ -11,3 +13,22 @@ def pickup_visualable_layers(model):
         if hasattr(layer,"weight") or hasattr(layer,"bias"):
             visualable_layers.append((name,layer))
     return visualable_layers
+
+def image_show(img):
+    """cifarのimageを見れるようにする モデルの可視化という本来の趣旨とは関係ない
+
+    Args:
+        img (_type_): _description_
+    """
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+def unite_weight_conv2d(weight_conv2d):
+    """nn.Conv2dの重みを結合する 縦方向がoutput横軸がカーネル数
+
+    Args:
+        weight_conv2d (_type_): _description_
+    """
+
+    return matrix
--- vismodel/visualize.py
+++ vismodel/visualize.py
@@ -1,6 +1,17 @@
 import matplotlib.pyplot as plt
-from utils import pickup_visualable_layers
+import torch.nn as nn
+from .utils import pickup_visualable_layers
 
+
+
+"""
+torch.nnモジュールで重みを持つ主なクラスは、torch.nn.Moduleを継承した以下のクラスです：
+Linear（線形層）: torch.nn.Linear
+Convolutional（畳み込み層）: torch.nn.Conv1d, torch.nn.Conv2d, torch.nn.Conv3d など
+Recurrent（再帰層）: torch.nn.RNN, torch.nn.LSTM, torch.nn.GRU など
+Embedding（埋め込み層）: torch.nn.Embedding
+Transformer（トランスフォーマー層）: torch.nn.TransformerEncoderLayer, torch.nn.TransformerDecoderLayer など
+"""
 
 def visualize_wandb(model):
 
@@ -13,5 +24,26 @@ def visualize_wandb(model):
             axes[0][i].imshow(layer.weight.detach().numpy(), cmap='viridis')
             axes[1][i].imshow(layer.bias.unsqueeze(1).detach().numpy(), cmap='viridis')
             #plt.colorbar()  # カラーバーを追加
+        elif isinstance(layer,nn.Conv1d):
+            weight_mat,bias_mat=layer.weight.unsqueeze(3),layer.bias.unsqueeze(1)
+            weight_mat=weight_mat.view(weight_mat.size(0)*weight_mat.size(1)*weight_mat.size(2),weight_mat.size(3))
+            axes[0][i].imshow(weight_mat.detach().numpy(), cmap='viridis')
+
+            bias_mat=layer.bias.unsqueeze(1)
+            axes[1][i].imshow(bias_mat.detach().numpy(), cmap='viridis')
+            pass
+
+        elif isinstance(layer,nn.Conv2d):
+            weight_mat,bias_mat=layer.weight,layer.bias
+            weight_mat=weight_mat.view(weight_mat.size(0)*weight_mat.size(1)*weight_mat.size(2),weight_mat.size(3))
+            axes[0][i].imshow(weight_mat.detach().numpy(), cmap='viridis')
+
+            bias_mat=layer.bias.unsqueeze(1)
+            axes[1][i].imshow(bias_mat.detach().numpy(), cmap='viridis')
+        elif isinstance(layer,nn.LSTM):
+            #n_layersの数だけ
+            for n in layer.num_layers:
+                #lstm.weight_ih_l0', 'lstm.weight_hh_l0', 'lstm.bias_ih_l0', 'lstm.bias_hh_l0'
+                pass
         else:
             print(name,layer)
